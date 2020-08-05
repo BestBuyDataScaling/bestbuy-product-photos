f47e0aae9e1a70634182c1de287764bb8c56bdb2
Mike Champagne <geauxdrum@gmail.com>
2020-08-05 17:30:38 -0500

    completes client boilerplate

---
+++ database/videoGameData.js
@@ -0,0 +1,24 @@
+const videoGameData = [
+  {
+    uniqueID: '',
+    name: '',
+    description: '',
+    brand: '',
+    department: '',
+    color: '',
+    subDept: '',
+    sku: '',
+    price: '',
+    avgRating: '',
+    colors: '',
+    reviews: [],
+    questions: {
+      question: '',
+      answer: ''
+    },
+    images: [],
+    peopleAlsoBought: [],
+    peopleAlsoViewed: [],
+    recentlyViewed: false
+  }
+]

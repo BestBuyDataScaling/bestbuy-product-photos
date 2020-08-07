78ec93771caaa19a060ae83ab36c0c92b9487e46
Mike Champagne <geauxdrum@gmail.com>
2020-08-07 16:34:22 -0500

    renders left hand column of item view

---
+++ client/src/components/ImageWrapper.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const ImageWrapper = (props) => {
+  return (
+    <div className="primary-button">
+      <img className="primary-image"
+        src="https://pisces.bbystatic.com/image2/BestBuy_US/images/products/5721/5721500_sd.jpg;maxHeight=640;maxWidth=550"
+      />
+    </div>
+  );
+};
+
+export default ImageWrapper;
+

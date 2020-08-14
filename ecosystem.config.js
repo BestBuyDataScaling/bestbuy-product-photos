4deaac1dc47b9e68a93c42e81ec29ecf0e57d38e
Mike Champagne <geauxdrum@gmail.com>
2020-08-13 21:49:54 -0500

    updates ecosystem file

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -11,7 +11,7 @@ module.exports = {
       ref: 'origin/master',
       repo: 'git@github.com:ATX-50-Team-Best-Buy/productInfo-photos_service.git',
       path: '/home/ubuntu/productInfo-photos_service',
-      'post-deploy': 'npm install && pm2 startOrRestart ecosystem.config.js'
+      'post-deploy': 'cd client && npm run build && cd .. && pm2 startOrRestart ecosystem.config.js'
     }
   }
 }

d082d0f3ef7bdd6fe147f626acf9a02fe97a4865
Mike Champagne <geauxdrum@gmail.com>
2020-08-13 21:39:33 -0500

    adds ecosystem file

---
+++ ecosystem.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  apps: [{
+    name: 'productInfo-photos_service',
+    script: './server/index.js'
+  }],
+  deploy: {
+    production: {
+      user: 'ubuntu',
+      host: 'ec2-18-217-216-28.us-east-2.compute.amazonaws.com',
+      key: '~/.ssh/front-end-capstone.pem',
+      ref: 'origin/master',
+      repo: 'git@github.com:ATX-50-Team-Best-Buy/productInfo-photos_service.git',
+      path: '/home/ubuntu/productInfo-photos_service',
+      'post-deploy': 'npm install && pm2 startOrRestart ecosystem.config.js'
+    }
+  }
+}
